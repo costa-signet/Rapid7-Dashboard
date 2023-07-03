3d86995a1e4f9971377a80e701555f2274f77578
costa-signet <13597919@Jewels.com>
2023-07-03 14:28:43 -0400

    file header

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, catchError, combineLatest, map, tap, throwError } from "rxjs";
+import { Reports } from "./Types/Reports";
 
 
 @Injectable({
@@ -7,7 +9,41 @@ import { BehaviorSubject } from "rxjs";
 })
 //Service to load app component
 export class AppService{
-    private filterSelectedSubject = new BehaviorSubject<number>(0);
+    private reportSelectionSubject = new BehaviorSubject<string>('2023-05-02-rapid7');     //hard code most recent report for now
     
+    private LIST_URL = 'https://kbt9dzwcd4.execute-api.us-east-2.amazonaws.com/list';
+
+    constructor (private http: HttpClient) { }
+    
+    rapid7Files$ = this.http.get<Reports[]>(this.LIST_URL).pipe(
+        tap(data => console.log('All: ', JSON.stringify(data))), 
+        catchError(this.handleError)
+    );
+
+    reportSelectionAction$ = this.reportSelectionSubject.asObservable();
+
+    selectedReport$ = combineLatest([this.rapid7Files$, this.reportSelectionAction$])
+        .pipe(
+            map(([rapid7Files, selectedReportName]) => 
+                rapid7Files.find(rapid7File => rapid7File.file === selectedReportName)),
+            tap(report => console.log('selected report', report))
+        );
+
+    selectedReportChange(selectedReportName: string): void{ 
+        this.reportSelectionSubject.next(selectedReportName);
+        console.log('selected rapid7 file name ', this.reportSelectionSubject.value);
+    }
+
+    private handleError(err: HttpErrorResponse){
+        let errorMessage = '';
+        if (err.error instanceof ErrorEvent){
+            errorMessage = `An error occured ${err.error.message}`;
+            }
+        else 
+            errorMessage = `Server returned code:` 
+                
+        console.error(errorMessage);
+        return throwError(errorMessage);
+    }
     
 }
